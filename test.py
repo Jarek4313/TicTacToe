bd0b20cd206851aedbca0efde900f931ebd4c6e3
Jarek Dziura <jarek4313@gmail.com>
2020-11-02 10:27:20 +0100

    Dodanie zadania dla Antoniny

--- test.py
+++ test.py
@@ -1,12 +1,27 @@
-listA = []
+# listA = []
 
 
-listA.append("JAREK")
-listA.append("Dziura")
+# listA.append("JAREK")
+# listA.append("Dziura")
 
-listA[0] += "*****"
-listA[1] += "*****"
-#listA[2] += "*****"
+# listA[0] += "*****"
+# listA[1] += "*****"
+# #listA[2] += "*****"
 
-for i in range(7):
-    print(i)
+# for i in range(7):
+#     print(i)
+
+class Gracz:
+    def __init__(self, imie, punkty):
+        self.imie = imie
+        self.punkty = punkty
+
+gracz_00 = Gracz("Jarek", 100)
+gracz_01 = Gracz("Antonina", 101)
+gracz_02 = Gracz("Jan", 150)
+gracz_03 = Gracz("Kowalski", 200)
+gracz_04 = Gracz("Nowak", 98)
+
+
+moja_lista = [gracz_00, gracz_01, gracz_02, gracz_03, gracz_04]
+
